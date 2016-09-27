800e3477ddcf568a39754b1d63f2376ab203a063
Omer Faruk Yolal <omeryol42@gmail.com>
2016-09-27 16:06:50 -0500

    Comments added

--- main.c
+++ main.c
@@ -19,6 +19,7 @@ int main() {
     printf("Enter the maximum number please: ");
     scanf("%d", &max);
     while(max < min){
+    //Not allowing user to input impossible case for max, which max is less than min
         printf("The maximum number you have entered cannot be less than minimum number.\n");
         printf("Please re-enter maximum number: ");
         scanf("%d", &max);
@@ -26,6 +27,8 @@ int main() {
     arr[10] = max;
     printf("Enter the median please: ");
     scanf("%d", &med);
+    //median which is middle number cannot be less than min or more than max so i am forcing user to
+    //enter suitable number
     while (med < min || med > max) {
         printf("Median cannot be less than min or more than max.\n");
         printf("Please re-enter median: ");
@@ -34,25 +37,30 @@ int main() {
     arr[5] = med;
     printf("Please enter the mode: ");
     scanf("%d", &mod);
+    //mod cannot be more than max number or less than min so i am forcing user to
+    //enter suitable number
     while(mod > max || mod < min){
         printf("Mode cannot be less than min or more than max.\n");
         printf("Please re-enter mode: ");
         scanf("%d", &mod);
     }
+    //assigning numbers before median to mod if mod is less than median
     if (mod < med){
         arr[1] = arr[2] = arr[3] = arr[4] = mod;
         arr[6] = arr[7] = med;
         arr[8] = arr[9] = max;
+    //assigning numbers after median to mod if mod is more than median
     }else if(mod > med){
         arr[9] = arr[6] = arr[7] = arr[8] = mod;
         arr[1] = arr[2] = min;
         arr[3] = arr[4] = med;
+    //if median and mode are equal filling the data set with that number
     }else{
         for (int i=1; i<10; i++) {
             arr[i] = mod;
         }
     }
-    
+    //Excluding impossible casses
     if((med == max && mod < med) || (med == min && mod > med)){
         printf("Impossible Data-Set");
     }else{
